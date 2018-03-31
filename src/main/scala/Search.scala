644af65c5f7b58baa1d50967c05edc6ea48082b4
nkudinov <oracle_developer@mail.ru>
2018-03-31 18:01:45 +0300

    Scannner has been added

--- src/main/scala/Search.scala
+++ src/main/scala/Search.scala
@@ -1,4 +1,4 @@
-import java.util.Properties
+import java.util.{Properties, Scanner}
 
 import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.http.{HttpRequest, HttpRequestInitializer}
@@ -10,41 +10,45 @@ import scala.util.{Failure, Success, Try}
 
 object Search extends App {
   val PROPERTIES_FILENAME = "youtube.properties"
-
-  val NUMBER_OF_VIDEOS_RETURNED:Long = 17
+  val NUMBER_OF_VIDEOS_RETURNED = 17:Long
 
   val properties = new Properties()
-  def init(): Unit ={
+  def searchByString(searchString:String): Unit ={
+    Try(properties.load(Search.getClass.getClassLoader.getResourceAsStream(PROPERTIES_FILENAME))) match {
+      case Failure(e) => throw new RuntimeException(s"Can not load $PROPERTIES_FILENAME because of $e")
+      case _ =>
+    }
 
+    val youtube = new YouTube.Builder( new NetHttpTransport()
+      , new JacksonFactory()
+      , new HttpRequestInitializer(){
+        override def initialize(request: HttpRequest): Unit = {
+
+        }
+      }
+    ).setApplicationName("My Project").build
+
+    val search = youtube.search().list("id")
+    val apiKey= properties.getProperty("youtube.apikey")
+    search.setKey(apiKey)
+    search.setQ(searchString)
+    search.setType("video")
+    search.setFields("items(id/kind,id/videoId)")
+    search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED)
+
+
+
+    val searchResponse = search.execute
+    val searchResult = searchResponse.getItems
+    import scala.collection.JavaConversions._
+    searchResult.toList.map(x=>getTitleAndViewCount(youtube,apiKey,x)).foreach(println)
   }
-  Try(properties.load(Search.getClass.getClassLoader.getResourceAsStream(PROPERTIES_FILENAME))) match {
-    case Failure(e) => throw new RuntimeException(s"Can not load $PROPERTIES_FILENAME because of $e")
-    case _ =>
+  def readUserInputAndExecute(): Unit ={
+    val scanner = new Scanner(System.in)
+    while(scanner.hasNextLine)
+      searchByString(scanner.nextLine)
   }
 
-  val youtube = new YouTube.Builder( new NetHttpTransport()
-                                   , new JacksonFactory()
-                                   , new HttpRequestInitializer(){
-                override def initialize(request: HttpRequest): Unit = {
-
-                }
-                }
-  ).setApplicationName("My Project").build
-
-  val search = youtube.search().list("id")
-  val apiKey= properties.getProperty("youtube.apikey")
-  search.setKey(apiKey)
-  search.setQ("москва весна 2018")
-  search.setType("video")
-  search.setFields("items(id/kind,id/videoId)")
-  search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED)
-
-
-
-  val searchResponse = search.execute
-  val searchResult = searchResponse.getItems
-  import scala.collection.JavaConversions._
-  searchResult.toList.map(x=>getTitleAndViewCount(youtube,apiKey,x)).foreach(println)
 
   def getTitleAndViewCount(yt: YouTube, key:String,v: SearchResult): (BigInt,String) ={
     val videoResponse = yt.videos().list("snippet,contentDetails,statistics").setId(v.getId.getVideoId).setKey(key).execute()
@@ -52,4 +56,5 @@ object Search extends App {
    (video.getStatistics.getViewCount,video.getSnippet.getTitle)
 
   }
+  readUserInputAndExecute
 }
