01a6e02f3076f41f23857274d976798462adce6c
nkudinov <oracle_developer@mail.ru>
2018-03-31 19:33:39 +0300

    Pure

--- src/main/scala/Search.scala
+++ src/main/scala/Search.scala
@@ -5,19 +5,24 @@ import com.google.api.client.http.{HttpRequest, HttpRequestInitializer}
 import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.services.youtube.YouTube
 import com.google.api.services.youtube.model.{SearchListResponse, SearchResult}
-
+import com.typesafe.config.ConfigFactory
+import pureconfig.error.ConfigReaderFailures
+import pureconfig.error.ConfigReaderFailures
+import pureconfig.loadConfig
+import pureconfig.loadConfigOrThrow
+import scala.collection.JavaConversions._
 import scala.util.{Failure, Success, Try}
-
+import com.typesafe.config.ConfigFactory
 object Search extends App {
-  val PROPERTIES_FILENAME = "youtube.properties"
-  val NUMBER_OF_VIDEOS_RETURNED = 17:Long
 
-  val properties = new Properties()
+  val config = loadConfigOrThrow[YouTubeConfig]
+
+  def readUserInputAndExecute(): Unit ={
+    val scanner = new Scanner(System.in)
+    while(scanner.hasNextLine)
+      searchByString(scanner.nextLine)
+  }
   def searchByString(searchString:String): Unit ={
-    Try(properties.load(Search.getClass.getClassLoader.getResourceAsStream(PROPERTIES_FILENAME))) match {
-      case Failure(e) => throw new RuntimeException(s"Can not load $PROPERTIES_FILENAME because of $e")
-      case _ =>
-    }
 
     val youtube = new YouTube.Builder( new NetHttpTransport()
       , new JacksonFactory()
@@ -26,29 +31,19 @@ object Search extends App {
 
         }
       }
-    ).setApplicationName("My Project").build
+    ).setApplicationName("youtube_cmd").build
 
     val search = youtube.search().list("id")
-    val apiKey= properties.getProperty("youtube.apikey")
-    search.setKey(apiKey)
+    search.setKey(config.apiKey)
     search.setQ(searchString)
     search.setType("video")
     search.setFields("items(id/kind,id/videoId)")
-    search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED)
-
-
-
+    search.setMaxResults(config.maxResults)
     val searchResponse = search.execute
     val searchResult = searchResponse.getItems
-    import scala.collection.JavaConversions._
-    searchResult.toList.map(x=>getTitleAndViewCount(youtube,apiKey,x)).foreach(println)
-  }
-  def readUserInputAndExecute(): Unit ={
-    val scanner = new Scanner(System.in)
-    while(scanner.hasNextLine)
-      searchByString(scanner.nextLine)
-  }
 
+    searchResult.toList.map(x=>getTitleAndViewCount(youtube,config.apiKey,x)).foreach(println)
+  }
 
   def getTitleAndViewCount(yt: YouTube, key:String,v: SearchResult): (BigInt,String) ={
     val videoResponse = yt.videos().list("snippet,contentDetails,statistics").setId(v.getId.getVideoId).setKey(key).execute()
